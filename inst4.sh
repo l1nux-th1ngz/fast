b58a75bcd002bf46efed6d5d1067d1a0cdf5cf07
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-19 16:05:01 -0700

    Update inst4.sh

--- inst4.sh
+++ inst4.sh
@@ -1,5 +1,162 @@
 #!/bin/bash
 
+# Check if script is run as root
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Check if whiptail is available
+if ! command -v whiptail >/dev/null; then
+    echo "whiptail is not installed. Install it and run this script again."
+    exit 1
+fi
+
+# Use whiptail to get user input
+USERNAME=$(whiptail --inputbox "Enter your username" 8 78 --title "User Input" 3>&1 1>&2 2>&3)
+PASSWORD=$(whiptail --passwordbox "Enter your password" 8 78 --title "User Input" 3>&1 1>&2 2>&3)
+
+# Create a 500MB EFI partition and a root partition with the rest of the space
+echo -e "g\nn\n\n\n+500M\nt\n1\nn\n\n\n\nw" | fdisk /dev/nvme0n1
+
+# Install base system and generate fstab
+pacstrap /mnt base base-devel linux linux-firmware
+genfstab -U /mnt > /mnt/etc/fstab
+
+# List of packages to install
+packages=(
+    gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 wayland
+    libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon 
+    xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio hyprland-git wlroots gcc clang
+    dunst mako cliphist pipewire wireplumber cava polkit-kde-agent qt5-wayland qt6-wayland rofi-lbonn-wayland-git
+    pacseek kate marker nano-syntax-highlighting pipewire-alsa pipewire-audio pipewire-jack	pipewire-pulse	
+    gst-plugin-pipewire	networkmanager network-manager-applet bluez bluez-utils	blueman brightnessctl impression 
+    libreoffice-fresh
+    )
+
+# Update the system first
+echo "Updating system..."
+sudo pacman -Syu
+
+# Install packages
+for pkg in "${packages[@]}"
+do
+    echo "Installing $pkg..."
+    yay -S --noconfirm "$pkg"
+done
+
+echo "All packages installed successfully."
+
+# Prepare for the next script
+sed '1,/^$/d' inst1.sh > /mnt/inst1.sh
+chmod +x /mnt/inst1.sh
+arch-chroot /mnt ./inst1.sh
+
+# Create .fonts directory in the home folder
+mkdir -p ~/.fonts
+
+# Initialize pacman keyring
+sudo pacman-key --init
+
+# Update font cache
+fc-cache
+
+# Install fontconfig, anyrun-git, and emacs-gcc-wayland-devel-bin using yay
+yay -S --noconfirm fontconfig anyrun-git emacs-gcc-wayland-devel-bin
+
+# Prepare for the next script
+sed '1,/^$/d' inst2.sh > /mnt/inst2.sh
+chmod +x /mnt/inst2.sh
+arch-chroot /mnt ./inst2.sh
+
+# List of packages to install
+packages=(
+    qt5-quickcontrols qt5-quickcontrols2 qt5-graphicaleffects swww swaylock-effects-git
+    swayidle wlogout grim slurp swappy xdg-desktop-portal-hyprland-git xdg-desktop-portal-gtk imv
+    qt5-imageformats pavucontrol pamixer nwg-look kvantum qt5ct firefox thunderbird neofetch dolphin
+    visual-studio-code-bin vim helix geany geany-plugins plymouth-git
+)
+
+# Update the system first
+echo "Updating system..."
+sudo pacman -Syu
+
+# Install packages
+for pkg in "${packages[@]}"
+do
+    echo "Installing $pkg..."
+    yay -S --noconfirm "$pkg"
+done
+
+echo "All packages installed successfully."
+
+# Prepare for the next script
+sed '1,/^$/d' inst3.sh > /mnt/inst3.sh
+chmod +x /mnt/inst3.sh
+arch-chroot /mnt ./inst3.sh
+
+# List of packages to install
+packages=(
+    ttf-sora zenity thefuck python-ansicolors lsd sox \
+    ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font \
+    ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa \
+    ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd python-pipx \
+    adobe-source-code-pro-fonts tidy mpd mpc python-pip \
+    pixman pango cairo mailspring emote \
+    aria2 udiskie udisks2 python-pip picom jq cantata \
+    ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa \
+    ttf-jetbrains-mono ttf-icomoon-feather xdg-desktop-portal-hyprland-git \
+    ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd \
+    ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font \
+    parallel socat starship \
+    otf-sora neovim eww-wayland \
+    ttf-nerd-fonts-symbols-common otf-firamono-nerd inter-font \
+    ttf-fantasque-nerd noto-fonts noto-fonts-emoji ttf-comfortaa \
+    ttf-jetbrains-mono-nerd ttf-icomoon-feather ttf-iosevka-nerd \
+    adobe-source-code-pro-fonts ttf-ms-win11-auto adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts \
+    ttf-jetbrains-mono otf-font-awesome nerd-fonts-sf-mono \
+    otf-nerd-fonts-monacob-mono ttf-font-awesome light brillo \
+    mint-themes yad impression \
+    fish spicetify zsh playerctl spotify noise-suppression-for-voice \
+    exa spotify-adblock-git tumbler \
+    oh-my-zsh spicetify-themes-git util-linux-libs \
+    zsh-theme-powerlevel10k compiler-rt \
+    zsh-syntax-highlighting python-compile blueman-applet \
+    zsh-autosuggestions spicetify-marketplace-bin \
+    pokemon-colorscripts-git genius-spicetify-git
+)
+
+# Update the system first
+echo "Updating system..."
+sudo pacman -Syu
+
+# Install packages
+for pkg in "${packages[@]}"
+do
+    echo "Installing $pkg..."
+    yay -S --noconfirm "$pkg"
+done
+
+echo "All packages installed successfully."
+
+# Enable TRIM
+sudo systemctl enable fstrim.timer
+
+# Clone and install Proxzima Plymouth theme
+git clone https://aur.archlinux.org/proxzima-plymouth-git.git && cd proxzima-plymouth && sudo cp -r proxzima /usr/share/plymouth/themes && sudo plymouth-set-default-theme -R proxzima
+
+# Enable Plymouth
+sudo systemctl enable plymouth
+
+# Download and install Nerd Font
+curl -fLo "<FONT_NAME> Nerd Font Complete.otf" \
+https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/<FONT_PATH>/complete/<FONT_NAME>%20Nerd%20Font%20Complete.otf
+
+# Prepare for the next script
+sed '1,/^$/d' inst4.sh > /mnt/inst4.sh
+chmod +x /mnt/inst4.sh
+arch-chroot /mnt ./inst4.sh
+
 # Backup the original pacman.conf file
 sudo cp /etc/pacman.conf /etc/pacman.conf.bak
 
@@ -44,63 +201,4 @@ CheckSpace
 VerbosePkgLists
 ParallelDownloads = 20
 
-# By default, pacman accepts packages signed by keys that its local keyring
-# trusts (see pacman-key and its man page), as well as unsigned packages.
-SigLevel    = Required DatabaseOptional
-LocalFileSigLevel = Optional
-#RemoteFileSigLevel = Required
-
-# NOTE: You must run `pacman-key --init` before first using pacman; the local
-# keyring can then be populated with the keys of all official Arch Linux
-# packagers with `pacman-key --populate archlinux`.
-
-#
-# REPOSITORIES
-#   - can be defined here or included from another file
-#   - pacman will search repositories in the order defined here
-#   - local/custom mirrors can be added here or in separate files
-#   - repositories listed first will take precedence when packages
-#     have identical names, regardless of version number
-#   - URLs will have $repo replaced by the name of the current repo
-#   - URLs will have $arch replaced by the name of the architecture
-#
-# Repository entries are of the format:
-#       [repo-name]
-#       Server = ServerName
-#       Include = IncludePath
-#
-# The header [repo-name] is crucial - it must be present and
-# uncommented to enable the repo.
-#
-
-# The testing repositories are disabled by default. To enable, uncomment the
-# repo name header and Include lines. You can add preferred servers immediately
-# after the header, and they will be used before the default mirrors.
-
-#[core-testing]
-#Include = /etc/pacman.d/mirrorlist
-
-[core]
-Include = /etc/pacman.d/mirrorlist
-
-#[extra-testing]
-#Include = /etc/pacman.d/mirrorlist
-
-[extra]
-Include = /etc/pacman.d/mirrorlist
-
-#[laur-testing]
-#Include = /etc/pacman.d/mirrorlist
-
-[laur]
-Include = /etc/pacman.d/mirrorlist
-
-[multilib]
-Include = /etc/pacman.d/mirrorlist
-
-# An example of a custom package repository.  See the pacman manpage for
-# tips on creating your own repositories.
-#[custom]
-#SigLevel = Optional TrustAll
-#Server = file:///home/custompkgs
-EOF
+# By default, pacman accepts packages signed by keys that its
