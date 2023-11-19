2d483b0fd7610047f369bf127b03d5f01a06041a
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-19 08:40:39 -0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -7,8 +7,9 @@ packages=(
     xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio hyprland-git wlroots gcc clang
     dunst mako cliphist pipewire wireplumber cava polkit-kde-agent qt5-wayland qt6-wayland rofi-lbonn-wayland-git
     pacseek kate marker nano-syntax-highlighting pipewire-alsa pipewire-audio pipewire-jack	pipewire-pulse	
-    gst-plugin-pipewire	networkmanager network-manager-applet bluez bluez-utils	blueman brightnessctl
-)
+    gst-plugin-pipewire	networkmanager network-manager-applet bluez bluez-utils	blueman brightnessctl impression 
+    libreoffice-fresh
+    )
 
 # Update the system first
 echo "Updating system..."
