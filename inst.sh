ecf6101887e7a64a28892c457e5e0760ada0a0a1
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-19 07:37:37 -0700

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -2,9 +2,12 @@
 
 # List of packages to install
 packages=(
-    gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 
+    gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 wayland
     libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon 
-    xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio hyprland-git
+    xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio hyprland-git wlroots gcc clang
+    dunst mako cliphist pipewire wireplumber cava polkit-kde-agent qt5-wayland qt6-wayland rofi-lbonn-wayland-git
+    pacseek kate marker nano-syntax-highlighting pipewire-alsa pipewire-audio pipewire-jack	pipewire-pulse	
+    gst-plugin-pipewire	networkmanager network-manager-applet bluez bluez-utils	blueman brightnessctl
 )
 
 # Update the system first
